3add8faf236ddea9af42c231c92a49272d6d971c
Parker <parkerlockhart42@gmail.com>
2021-10-18 20:02:39 -0500

    Add health method test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,8 +1,16 @@
-require '.lib/ship'
+require './lib/ship'
 
 describe Ship do
   it 'exists'do
     cruiser = Ship.new("Cruiser", 3)
     expect(cruiser).to be_instance_of(Ship)
   end
+
+  describe '#health' do
+    it 'returns ship health status' do
+      cruiser = Ship.new("Cruiser", 3)
+      expect(cruiser.health).to eq(3)
+    end
+  end
+
 end
