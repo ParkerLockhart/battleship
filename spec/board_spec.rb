5a9d877479dc366bbf60b3ca34c156038786afda
Parker <parkerlockhart42@gmail.com>
2021-10-20 19:09:18 -0500

    Add cells test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Board do
     board = Board.new
     expect(board).to be_instance_of(Board)
   end
+
+  it 'has cells' do
+    board = Board.new
+    expect(board.cells).to be_a Hash
+    expect(board.cells.size).to eq(16)
+  end
 end
