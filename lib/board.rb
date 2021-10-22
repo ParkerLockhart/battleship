ee2b92d89e5cbeb7744fe4f81345675dd018d6c3
Sam Iavri <sammi80134@gmail.com>
2021-10-21 19:28:33 -0600

    Validates adjacentcy and linearness

--- lib/board.rb
+++ lib/board.rb
@@ -43,4 +43,48 @@ class Board
 
 #.sort to put coordinates in order
 
+
+  def adjacent?(cell_1, cell_2)
+    # cell_1 is like "A1"
+    # cell_2 is like "A2"
+    # is the absolute value of the difference between the cell numbers equal to 1?
+    row_adjacent?(cell_1, cell_2) || column_adjacent?(cell_1, cell_2)
+
+  end
+
+  def row_adjacent?(cell_1, cell_2)
+    (cell_1[1].to_i - cell_2[1].to_i).abs == 1
+  end
+
+  def column_adjacent?(cell_1, cell_2)
+    (cell_1[0].ord - cell_2[0].ord).abs == 1
+  end
+
+  def same_row?(coordinates)
+    if coordinates.count == 2
+      coordinates[0][0] == coordinates[1][0]
+    else
+      (coordinates[0][0] == coordinates[1][0]) && (coordinates[1][0] == coordinates[2][0])
+    end
+  end
+
+  def same_column?(coordinates)
+    (coordinates[0][1] == coordinates[1][1]) && (coordinates[1][1] == coordinates[2][1])
+  end
+
+  def linear?(coordinates)
+    # coordinates is sometimes like [x, y, z] and sometimes like [x, y]
+
+    # 1: Are the pairs all adjacent?
+    # 2: Are the coordinates all on the same row or column
+
+    if coordinates.count == 2
+      adjacent?(coordinates[0], coordinates[1])
+    else
+      (adjacent?(coordinates[0], coordinates[1]) && adjacent?(coordinates[1], coordinates[2])) && (same_row?(coordinates) || same_column?(coordinates))
+    end
+
+  end
+
+
 end
