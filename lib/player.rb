55aaff7e16e6213e5efddca6761f52cd7e414be8
Sam Iavri <sammi80134@gmail.com>
2021-10-25 14:44:35 -0600

    Adds code into ship_placement method

--- lib/player.rb
+++ lib/player.rb
@@ -20,11 +20,20 @@ class Player
       B . . . .
       C . . . .
       D . . . ."
+    p prompt
     p ship_placement
   end
 
   def ship_placement
-    
+
+    p "Enter the squares for the Cruiser (3 spaces):"
+    user_placement = gets.to_s.chomp
+    if board.valid_placement? == false
+      p "Invalid placement, try again."
+    else board.valid_placement? == true
+      board.place(@cruiser, user_placement.split)
+    end
+  
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -11,12 +11,20 @@ RSpec.describe Player do
   end
 
   it 'prompts the player to place their ships' do
-  player = Player.new
+    player = Player.new
 
   expect(player.setup).to eq(prompt)
   end
 
-  it 'lets the player place three ships on the board' do
-    
+  it 'lets the player place a ship on the board' do
+  player = Player.new
+  board = Board.new
+  cruiser = Ship.new("Cruiser", 3)
+  board.place(cruiser, ["C2", "C3", "C4"])
+  cell_1 = board.cells["C2"]
+  cell_2 = board.cells["C3"]
+  cell_3 = board.cells["C4"]
+  expect(player.ship_placement).to eq(cell_1.ship, cell_2.ship, cell_3.ship)
+
   end
 end
