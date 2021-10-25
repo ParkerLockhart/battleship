20b57cf0021720119a50a1428667ab880789aa5e
Sam Iavri <sammi80134@gmail.com>
2021-10-25 11:42:47 -0600

    Initialized the player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,12 @@ require './lib/cell'
 require './lib/ship'
 require './lib/board'
 require './lib/gameplay'
+require './lib/player'
 
-Rspec.describe Player do
+RSpec.describe Player do
   it 'exists' do
     player = Player.new
     expect(player).to be_instance_of(Player)
   end
-end 
+  
+end
