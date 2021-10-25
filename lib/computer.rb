358e6080eef04ad24d7022f067f6af71e639a8ff
Parker <parkerlockhart42@gmail.com>
2021-10-25 16:59:14 -0500

    Add computer place ship methods

---
+++ lib/computer.rb
@@ -0,0 +1,38 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+
+class Computer
+  attr_reader :board, :cruiser, :submarine
+
+  def initialize
+    @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+  end
+
+  def setup_cruiser
+    horiz = [["A1", "A2", "A3"], ["A2", "A3", "A4"], ["B1", "B2", "B3"], ["B2", "B3", "B4"], ["C1", "C2", "C3"], ["C2", "C3", "C4"], ["D1", "D2", "D3"], ["D2", "D3", "D4"]]
+    vert = [["A1", "B1", "C1"], ["B1", "C1", "D1"], ["A2", "B2", "C2"], ["B2", "C2", "D2"], ["A3", "B3", "C3"], ["B3", "C3", "D3"], ["A4", "B4", "C4"], ["B4", "C4", "D4"]]
+    possible = horiz + vert
+
+    possible.keep_if do |coordinates|
+      board.valid_placement?(@cruiser, coordinates)
+    end
+
+    board.place(@cruiser, possible.sample)
+  end
+
+  def setup_submarine
+    horiz = [["A1", "A2"], ["A2", "A3"], ["A3", "A4"], ["B1", "B2"], ["B2", "B3"], ["B3", "B4"], ["C1", "C2"], ["C2", "C3"], ["C3", "C4"], ["D1", "D2"], ["D2", "D3"], ["D3", "D4"]]
+    vert = [["A1", "B1"], ["B1", "C1"], ["C1", "D1"], ["A2", "B2"], ["B2", "C2"], ["C2", "D2"], ["A3", "B3"], ["B3", "C3"], ["C3", "D3"], ["A4", "B4"], ["B4", "C4"], ["C4", "D4"]]
+    possible = horiz + vert
+
+    possible.keep_if do |coordinates|
+      board.valid_placement?(@submarine, coordinates)
+    end
+
+    board.place(@submarine, possible.sample)
+  end 
+end
