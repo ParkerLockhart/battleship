1b7344d200d976d494b48664032d1757fd367176
Parker <parkerlockhart42@gmail.com>
2021-10-25 11:13:57 -0500

    Add class and start tests

--- spec/gameplay_spec.rb
+++ spec/gameplay_spec.rb
@@ -8,4 +8,19 @@ RSpec.describe Gameplay do
     gameplay = Gameplay.new
     expect(gameplay).to be_instance_of(Gameplay)
   end
+
+  it '#greeting' do
+    gameplay = Gameplay.new
+    expect(gameplay.greeting).to eq("Welcome to BATTLESHIP" + "Enter p to play. Enter q to quit.")
+  end
+
+  it '#start' do
+
+  end
+
+  it '#setup' do
+
+  end
+
+
 end
