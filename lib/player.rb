1f33887fa8bbb11c634ae8a572efba17de4cf275
Sam Iavri <sammi80134@gmail.com>
2021-10-26 14:31:16 -0600

    reformats player.setup

--- lib/player.rb
+++ lib/player.rb
@@ -12,15 +12,15 @@ class Player
   end
 
   def setup
-    prompt = "I have laid out my ships on the grid.
-    You now need to lay out your two ships.
-    The Cruiser is three units long and the Submarine is two units long.
+    puts "I have laid out my ships on the grid."
+    puts "You now need to lay out your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long.
         1 2 3 4
       A . . . .
       B . . . .
       C . . . .
       D . . . ."
-    puts prompt
+
     ship_placement
   end
 
@@ -66,5 +66,5 @@ class Player
   def display_board
     puts "==============PLAYER BOARD=============="
     puts board.render(true)
-  end 
+  end
 end
