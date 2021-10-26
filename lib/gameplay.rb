edfeabf0ad56392119097df840af58f4091cb5e3
Sam Iavri <sammi80134@gmail.com>
2021-10-25 19:53:06 -0600

    Player takes a shot

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -21,11 +21,40 @@ class Gameplay
     if user_input == "p"
       puts "Lets go!"
       player.setup
+      play
     elsif user_input == "q"
       end_game
     else
       puts "Error: invalid input. Please enter p to play or q to quit."
     end
+  end
+
+  def play
+    puts ""
+    puts "Let's play!"
+    puts ""
+
+    #puts computer.board.render
+    puts player.board.render(true)
+
+    player_shoots
+
+  end
+
+  def player_shoots
+    valid_shot = false
+
+    puts "Enter the coordinate for your shot:"
+    until valid_shot
+      user_input = gets.chomp
+      if player.board.valid_coordinate?(user_input)
+        valid_shot = true
+      else
+        puts "Please enter a valid coordinate:"
+      end
+    end
+
+
   end
 
   def end_game
