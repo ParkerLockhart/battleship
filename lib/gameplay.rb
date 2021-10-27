83395023f799d0e2a5bc23547bea6512ffbd82a9
Sam Iavri <sammi80134@gmail.com>
2021-10-26 19:14:54 -0600

    refactore gamplay with extract method

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -11,28 +11,36 @@ class Gameplay
   end
 
   def start
-    puts "Welcome to BATTLESHIP"
-    puts "Enter p to play. Enter q to quit."
+    puts greeting
+    puts prompt_to_play
     user_input = gets.chomp
 
-    if user_input == "p"
-      puts "Lets go!"
+    if user_input == 'p'
+      puts 'Lets go!'
       computer.setup
       player.setup
       turn
-    elsif user_input == "q"
+    elsif user_input == 'q'
       exit
     else
-      puts "Error: invalid input. Please enter p to play or q to quit."
+      puts 'Error: invalid input. Please enter p to play or q to quit.'
       start
     end
+  end
+
+  def greeting
+    'Welcome to BATTLESHIP'
+  end
+
+  def prompt_to_play
+    'Enter p to play. Enter q to quit.'
   end
 
   def turn
     until player.all_ships_sunk? || computer.all_ships_sunk?
       valid_shot = false
 
-      puts "Enter the coordinate for your shot:"
+      puts 'Enter the coordinate for your shot:'
       until valid_shot
         user_input = gets.chomp
         if computer.board.valid_coordinate?(user_input) && computer.board.cells[user_input].fired_upon? == false
@@ -40,37 +48,34 @@ class Gameplay
         elsif computer.board.valid_coordinate?(user_input) && computer.board.cells[user_input].fired_upon
           puts "You have already fired on that coordinate. Please choose a coordinate you haven't already fired upon:"
         else
-          puts "Please enter a valid coordinate:"
+          puts 'Please enter a valid coordinate:'
         end
       end
 
       computer.board.fire_upon(user_input)
 
-
       shot = player.board.cells.keys.sample
 
-      while player.board.cells[shot].fired_upon?
-        shot = player.board.cells.keys.sample
-      end
+      shot = player.board.cells.keys.sample while player.board.cells[shot].fired_upon?
 
       player.board.fire_upon(shot)
 
       computer.display_board
       player.display_board
 
-      if computer.board.cells[user_input].render == "X"
+      if computer.board.cells[user_input].render == 'X'
         puts "Your shot on #{user_input} sunk my ship!"
-      elsif computer.board.cells[user_input].render == "H"
+      elsif computer.board.cells[user_input].render == 'H'
         puts "Your shot on #{user_input} was a hit!"
-      elsif computer.board.cells[user_input].render == "M"
+      elsif computer.board.cells[user_input].render == 'M'
         puts "Your shot on #{user_input} was a miss."
       end
 
-      if player.board.cells[shot].render == "X"
+      if player.board.cells[shot].render == 'X'
         puts "My shot on #{shot} sunk your ship!"
-      elsif player.board.cells[shot].render == "H"
+      elsif player.board.cells[shot].render == 'H'
         puts "My shot on #{shot} was a hit!"
-      elsif player.board.cells[shot].render == "M"
+      elsif player.board.cells[shot].render == 'M'
         puts "My shot on #{shot} was a miss."
       end
     end
@@ -78,10 +83,10 @@ class Gameplay
   end
 
   def end_game
-    if player.board.ships.all?{|ship| ship.sunk?}
-      puts "I won!"
-    elsif computer.board.ships.all?{|ship| ship.sunk?}
-      puts "You won!"
+    if player.board.ships.all? { |ship| ship.sunk? }
+      puts 'I won!'
+    elsif computer.board.ships.all? { |ship| ship.sunk? }
+      puts 'You won!'
     end
     start
   end
